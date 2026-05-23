480210112f505a3008935b9e451d2238aa003bbb
hackerboy2122 <hackerboy2221111111@gmail.com>
2026-05-23 13:44:18 +0530

    Added keep-alive express server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is active!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 const { Telegraf, Markup } = require('telegraf');
 const axios = require('axios');
 
